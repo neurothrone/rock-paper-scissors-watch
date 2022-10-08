dddb31d18cd30ba7bd5d3825ffc95604db2fa010
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-10-08 18:38:05 +0200

    Initial Commit

---
+++ RockPaperScissors Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RockPaperScissors Watch App
+//
+//  Created by Zaid Neurothrone on 2022-10-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RockPaperScissors Watch App/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors Watch App
+//
+//  Created by Zaid Neurothrone on 2022-10-08.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissors_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
